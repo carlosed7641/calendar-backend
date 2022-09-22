27eae8d6c73db334c5569c2103d0faa4defc2c07
Carlos Castro <carlosed7641@gmail.com>
2022-09-22 13:00:37 -0500

    crud finish

--- controllers/events.js
+++ controllers/events.js
@@ -1,43 +1,126 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
+const getEvents = async(req, res = response) => {
 
-const getEvents = (req, res = response) => {
+ const eventos = await Evento.find()
+                                .populate('user', 'name') ; //Para traer info del user
+
+  res.json({
+    ok: true,
+    eventos
+  });
+};
+
+const createEvent = async (req, res = response) => {
+  const evento = new Evento(req.body);
+
+  try {
+    evento.user = req.uid;
+
+    const saveEvent = await evento.save();
+
     res.json({
-        ok: true, 
-        msg: 'getEventos'
-    })
-}
+      ok: true,
+      event: saveEvent,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el admin",
+    });
+  }
+};
 
-const createEvent = (req, res = response) => {
+const updateEvent = async(req, res = response) => {
 
-    //Verificar que tenga el evento
-    console.log(req.body);
-    
+ const eventoId = req.params.id;
+ const uid = req.uid;
+
+ try {
+    const event = await Evento.findById(eventoId);
+
+    if (!event) {
+        res.status(404).json({
+            ok: false,
+            msg: 'No existe con ese id'
+        })
+    }
+
+    if (event.user.toString() !== uid) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No tiene permisos para editar este evento'
+        })
+    }
+
+    const nuevoEvento = {
+        ...req.body,
+        user: uid
+    }
+
+    const updatedEvent = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true});
 
     res.json({
-        ok: true, 
-        msg: 'create'
+        ok: true,
+        evento: updatedEvent
     })
-}
 
-const updateEvent = (req, res = response) => {
-    res.json({
-        ok: true, 
-        msg: 'update'
+
+
+ } catch (error) {
+    console.log(error);
+    res.status(500).json({
+        ok: false,
+        msg: 'Hable con el admin'
     })
-}
+ }
+};
 
-const deleteEvent = (req, res = response) => {
-    res.json({
-        ok: true, 
-        msg: 'eliminar'
-    })
-}
+const deleteEvent = async(req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+   
+    try {
+       const event = await Evento.findById(eventoId);
+   
+       if (!event) {
+           return res.status(404).json({
+               ok: false,
+               msg: 'No existe con ese id'
+           })
+       }
+   
+       if (event.user.toString() !== uid) {
+           return res.status(401).json({
+               ok: false,
+               msg: 'No tiene permisos para eliminar este evento'
+           })
+       }
+   
 
+       await Evento.findByIdAndDelete(eventoId);
+   
+       res.json({
+           ok: true,
+           msg: 'Evento eliminado'
+       })
+   
+   
+   
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({
+           ok: false,
+           msg: 'Hable con el admin'
+       })
+    }
+};
 
 module.exports = {
-    getEvents,
-    createEvent,
-    updateEvent,
-    deleteEvent
-}
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+};
--- models/Evento.js
+++ models/Evento.js
@@ -3,24 +3,32 @@ const { Schema, model } = require("mongoose");
 const EventoSchema = Schema({
   title: {
     type: String,
-    require: true,
+    required: true,
   },
   notes: {
     type: String,
   },
   start: {
     type: Date,
-    require: true,
+    required: true,
   },
   end: {
     type: Date,
-    require: true,
+    required: true,
   },
   user: {
     type: Schema.Types.ObjectId,
-    //Hace referencia al otro schema
-    ref: 'Usuario'
+    ref: 'Usuario', //Hace referencia al otro schema
+    required: true 
   }
 });
+
+EventoSchema.method('toJSON', function() {
+    const { __v, _id, ...object} = this.toObject();
+    object.id = _id;
+    return object;
+})
+
+
 
 module.exports = model("Evento", EventoSchema);
